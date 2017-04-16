e8fdb5e15ff4b0e72b54272d6791f9401407aece
Aleksandr Cherkas <aleksandr_cherkas@epam.com>
2017-04-16 14:35:47 +0300

    added expression transformer for known parameters

--- Solutions/1. Expression Trees/ExpressionTrees/ExpressionTransformer/IncDecTransformer.cs
+++ Solutions/1. Expression Trees/ExpressionTrees/ExpressionTransformer/IncDecTransformer.cs
@@ -4,27 +4,30 @@ using System.Linq.Expressions;
 
 namespace ExpressionTransformer
 {
-    public class Transformer : ExpressionVisitor
+    public class IncDecTransformer : ExpressionVisitor
     {
         protected override Expression VisitBinary(BinaryExpression node)
         {
-            if(node.NodeType == ExpressionType.Add || node.NodeType == ExpressionType.Subtract && node.Right.NodeType == ExpressionType.Constant && ((int)((ConstantExpression)node.Right).Value) == 1)
+            if ((node.NodeType == ExpressionType.Add || node.NodeType == ExpressionType.Subtract)
+                && node.Right.NodeType == ExpressionType.Constant
+                && ((int)((ConstantExpression)node.Right).Value) == 1)
             {
                 return node.NodeType == ExpressionType.Add ? Expression.Increment(node.Left) : Expression.Decrement(node.Left);
             }
+
             return base.VisitBinary(node);
         }
     }
 
     [TestClass]
-    public class UnitTest1
+    public class IncDecTransformerTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void IncDecTransformerTestMethod()
         {
             var intValue = 10;
             Expression<Func<int>> source = () => intValue - 1;
-            var convertedExpression = new Transformer().VisitAndConvert(source, string.Empty);
+            var convertedExpression = new IncDecTransformer().VisitAndConvert(source, string.Empty);
             var result = convertedExpression.Compile().Invoke();
             var sourceResult = source.Compile().Invoke();
             Assert.AreEqual(result, sourceResult);
