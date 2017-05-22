f91abac13776c395290d630aad018c1b47a2a564
Aleksandr Cherkas <aleksandr_cherkas@epam.com>
2017-05-22 10:19:32 +0300

    Added implementation of windows service

---
+++ Tasks/WindowsService/WindowsService/FileConcatenationService.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using MigraDoc.DocumentObjectModel;
+using MigraDoc.DocumentObjectModel.Shapes;
+using MigraDoc.Rendering;
+
+namespace WindowsService
+{
+    class FileConcatenationService
+    {
+        private const string OutputFile = "out.pdf";
+        private const int FileOpenAttemptsCount = 3;
+
+        private readonly FileSystemWatcher _watcher;
+
+        private readonly string _inputDirectoryName;
+        private readonly string _outputDirectoryName;
+
+        private readonly Thread _workingThread;
+        private readonly ManualResetEvent _workStop;
+        private readonly AutoResetEvent _newFile;
+
+        public FileConcatenationService(string inputDirectoryName, string outputDirectoryName)
+        {
+            _inputDirectoryName = inputDirectoryName;
+            _outputDirectoryName = outputDirectoryName;
+
+            if (!Directory.Exists(inputDirectoryName))
+                Directory.CreateDirectory(inputDirectoryName);
+
+            if (!Directory.Exists(outputDirectoryName))
+                Directory.CreateDirectory(outputDirectoryName);
+
+            _workingThread = new Thread(WorkProc);
+            _workStop = new ManualResetEvent(false);
+            _newFile = new AutoResetEvent(false);
+
+            _watcher = new FileSystemWatcher(inputDirectoryName);
+            _watcher.Created += FileCreated;
+        }
+
+        private void WorkProc()
+        {
+            do
+            {
+                Document document = new Document();
+                Section section = document.AddSection();
+                foreach (var file in Directory.EnumerateFiles(_inputDirectoryName))
+                {
+                    if (_workStop.WaitOne(TimeSpan.Zero))
+                    {
+                        return;
+                    }
+
+                    if (TryOpen(file, FileOpenAttemptsCount))
+                    {
+                        CreatePdf(file, document, section);
+                    }
+                }
+
+                var render = new PdfDocumentRenderer();
+                render.Document = document;
+                render.RenderDocument();
+                render.Save(Path.Combine(_outputDirectoryName, OutputFile));
+            }
+            while (WaitHandle.WaitAny(new WaitHandle[] { _workStop, _newFile }) != 0);
+        }
+
+        private void FileCreated(object sender, FileSystemEventArgs e)
+        {
+            _newFile.Set();
+        }
+
+        private bool TryOpen(string fullPath, int fileOpenAttemptsCount)
+        {
+            for (int attemptNumber = 0; attemptNumber < fileOpenAttemptsCount; attemptNumber++)
+            {
+                try
+                {
+                    var file = File.Open(fullPath, FileMode.Open, FileAccess.Read, FileShare.None);
+                    file.Close();
+                    return true;
+                }
+                catch (IOException)
+                {
+                    Thread.Sleep(3000);
+                }
+            }
+
+            return false;
+        }
+
+        public void CreatePdf(string file, Document document, Section section)
+        {
+            var img = section.AddImage(file);
+
+            img.RelativeHorizontal = RelativeHorizontal.Page;
+            img.RelativeVertical = RelativeVertical.Page;
+
+            img.Top = 0;
+            img.Left = 0;
+
+            img.Height = document.DefaultPageSetup.PageHeight;
+            img.Width = document.DefaultPageSetup.PageWidth;
+
+            section.AddPageBreak();
+        }
+
+        public void Start()
+        {
+            _workingThread.Start();
+            _watcher.EnableRaisingEvents = true;
+        }
+
+        public void Stop()
+        {
+            _watcher.EnableRaisingEvents = false;
+            _workStop.Set();
+            _workingThread.Join();
+        }
+    }
+}
---
+++ Tasks/WindowsService/WindowsService/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+using Topshelf;
+
+namespace WindowsService
+{
+    class Program
+    {
+        private const string InputFolderName = "in";
+        private const string OutputFolderName = "out";
+
+        static void Main(string[] args)
+        {
+            var currentDirectory = Directory.GetCurrentDirectory();
+
+            var inputDirectoryPath = Path.Combine(currentDirectory, InputFolderName);
+            var outputDirectoryPath = Path.Combine(currentDirectory, OutputFolderName);
+
+            Console.WriteLine(
+                $"Put images or Pdf documents into {inputDirectoryPath}.{Environment.NewLine}Result you can see in {outputDirectoryPath}");
+
+            var logConfig = new LoggingConfiguration();
+            var target = new FileTarget
+            {
+                Name = "Def",
+                FileName = Path.Combine(currentDirectory, "log.txt"),
+                Layout = "${date} ${message} ${onexception:inner=${exception:format=toString}}"
+            };
+
+            logConfig.AddTarget(target);
+            logConfig.AddRuleForAllLevels(target);
+
+            var logFactory = new LogFactory(logConfig);
+
+            HostFactory.Run(
+                conf => conf.Service<FileConcatenationService>(
+                    serv =>
+                    {
+                        serv.ConstructUsing(() => new FileConcatenationService(inputDirectoryPath, outputDirectoryPath));
+                        serv.WhenStarted(s => s.Start());
+                        serv.WhenStopped(s => s.Stop());
+                    }
+                ).UseNLog(logFactory)
+            );
+        }
+    }
+}
